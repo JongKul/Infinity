0f7cdb39c7619834490ed501ec5e990c0feb07f5
JongKul <leejk86@hanmail.net>
2013-04-01 20:52:53 +0900

    android facebook_binder
    기호 니때메 make 파일 생성  script도 만들었어

---
+++ Infinity/Classes/Social/Facebook/Binder/Facebook_Binder_Android.cpp
@@ -0,0 +1,34 @@
+//
+//  Facebook_Binder_Android.cpp
+//  Chess
+//
+//  Created by 占쏙옙疫꿸퀬��on 13. 3. 21..
+//
+//
+
+#include "Facebook_Binder_Android.h"
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+
+
+
+void Facebook_Binder_Android::Login()
+{
+
+}
+
+void Facebook_Binder_Android::GetPicture(cocos2d::CCString *fbID)
+{
+
+}
+
+void Facebook_Binder_Android::Invtie(cocos2d::CCString *fbID)
+{
+
+}
+
+void Facebook_Binder_Android::Post()
+{
+
+}
+
+#endif
