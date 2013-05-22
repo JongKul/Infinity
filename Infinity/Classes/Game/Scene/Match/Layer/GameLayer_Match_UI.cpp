c5bb8718b2d146ee8941be6bd0237b0853d816f4
정 기호 <bwjdrl@gmail.com>
2013-05-22 10:09:35 +0900

    giho - 이름추

--- Infinity/Classes/Game/Scene/Match/Layer/GameLayer_Match_UI.cpp
+++ Infinity/Classes/Game/Scene/Match/Layer/GameLayer_Match_UI.cpp
@@ -10,6 +10,8 @@
 #include "GameScene_Match.h"
 #include "Input_Manager.h"
 #include "Match_Map.h"
+#include "Room_Manager.h"
+#include "Facebook_Manager.h"
 
 bool GameLayer_Match_UI::init()
 {
@@ -20,6 +22,39 @@ bool GameLayer_Match_UI::init()
     
     wCount1 = AddSprite(ccp(600, 1280 - 155));
     wCount2 = AddSprite(ccp(660, 1280 - 155));
+    
+    Room_List* room = Room_Manager::sharedInstance()->curMatchRoom;
+    CCString* myAccount = Facebook_Manager::sharedInstance()->getMyAccount()->fbID;
+    if(room->white->isEqual(myAccount) == false)
+    {
+        CCLabelTTF *white = CCLabelTTF::create(room->other_user_Name->getCString(), "Helvetica", 30.0);
+        white->setPosition(ccp(250.0f, 1280.0f - 150.0f));
+        white->setAnchorPoint(ccp(0.0f,0.5f));
+        white->setColor(ccBLACK);
+        this->addChild(white);
+        
+        CCLabelTTF *black = CCLabelTTF::create(Facebook_Manager::sharedInstance()->getMyAccount()->name->getCString(), "Helvetica", 30.0);
+        black->setPosition(ccp(250.0f, 200.0f));
+        black->setAnchorPoint(ccp(0.0f,0.5f));
+        black->setColor(ccBLACK);
+        this->addChild(black);
+    }
+    else
+    {
+        CCLabelTTF *white = CCLabelTTF::create(Facebook_Manager::sharedInstance()->getMyAccount()->name->getCString(), "Helvetica", 30.0);
+        white->setPosition(ccp(250.0f, 1280.0f - 150.0f));
+        white->setAnchorPoint(ccp(0.0f,0.5f));
+        white->setColor(ccBLACK);
+        this->addChild(white);
+        
+        CCLabelTTF *black = CCLabelTTF::create(room->other_user_Name->getCString(), "Helvetica", 30.0);
+        black->setPosition(ccp(250.0f, 150.0f));
+        black->setAnchorPoint(ccp(0.0f,0.5f));
+        black->setColor(ccBLACK);
+        this->addChild(black);
+    }
+    
+    
     
     /*
     CCSize winSize = CCDirector::sharedDirector()->getWinSize();
